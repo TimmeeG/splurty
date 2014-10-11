13b47c3399f0871ad9b576315c3cf17011f9fad4
TimmeeG <tim.gallo@gmail.com>
2014-10-11 17:56:08 +0000

    added validation

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+	validates :saying, :presence => true, :length => {:maximum => 140, :minimum => 10}
+	validates :saying, :presence => true, :length => {:maximum => 50, :minimum => 5}
 end
